90b3d0e01fd3e9c95eb1841fa9492a81aede07c1
Alexandre PICKEL <a.pickel@student.helmo.be>
2022-03-04 12:01:13 +0100

    Execute SIGNUP

--- FileFrontEnd/src/main/java/secCon/PickLayaDeti/domains/tasks/client/SignUpTask.java
+++ FileFrontEnd/src/main/java/secCon/PickLayaDeti/domains/tasks/client/SignUpTask.java
@@ -1,25 +1,36 @@
 package secCon.PickLayaDeti.domains.tasks.client;
 
 import secCon.PickLayaDeti.domains.tasks.interfaces.TaskManager;
+import secCon.PickLayaDeti.security.AesKeyManager;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SignUpTask implements TaskManager {
+    private final AesKeyManager keyManager;
+    private Matcher matcher;
+
+    public SignUpTask() {
+        this.keyManager = new AesKeyManager();
+    }
+
     @Override
     public boolean check(String message) {
         Pattern pattern = Pattern.compile("^(SIGNUP) ([a-zA-Z0-9]{5,20}) ([a-zA-Z0-9]{5,50})$");
-        Matcher matcher = pattern.matcher(message);
+        this.matcher = pattern.matcher(message);
         return matcher.matches();
     }
 
     @Override
     public void execute(String message) {
-        // 1. Desplit le regex
-        // 2. Verifier si login existe : Si oui, sortie
-        // 3. Générer clé AES
-        // 4. Hachage mdp (SHA-384)
-        // 5. Ajout clé + login + mdp haché dans fichier Json
-
+        // Génération de la clé AES.
+        try {
+            var login = matcher.group(1);
+            var clearTextPassword = matcher.group(2);
+            var key = keyManager.generateAesKey();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
