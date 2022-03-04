606b2f251b61397b0502d7b0252674a46378873a
Alexandre PICKEL <a.pickel@student.helmo.be>
2022-03-04 09:00:25 +0100

    Regex 100% OK FFE Regex 100% OK SBE

---
+++ FileFrontEnd/src/main/java/secCon/PickLayaDeti/domains/tasks/interfaces/TaskManager.java
@@ -0,0 +1,9 @@
+package secCon.PickLayaDeti.domains.tasks.interfaces;
+
+public interface TaskManager {
+
+    boolean check(String message);
+
+    void execute(String message);
+
+}
---
+++ StorBackEnd/src/main/java/secCon/PickLayaDeti/tasks/EraseFileTask.java
@@ -0,0 +1,20 @@
+package secCon.PickLayaDeti.tasks;
+
+import secCon.PickLayaDeti.tasks.interfaces.TaskManager;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EraseFileTask implements TaskManager {
+    @Override
+    public boolean check(String message) {
+        Pattern pattern = Pattern.compile("^(ERASEFILE) ([a-zA-Z0-9]{50,200})$");
+        Matcher matcher = pattern.matcher(message);
+        return matcher.matches();
+    }
+
+    @Override
+    public void execute(String message) {
+
+    }
+}
