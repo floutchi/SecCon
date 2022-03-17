3454c7668b599b40cb476edd6c427e21cc18d926
E200918 <s.layachi@student.helmo.be>
2022-03-17 14:10:46 +0100

    Methode safeDelete

--- StorBackEnd/src/main/java/secCon/PickLayaDeti/tasks/interfaces/TaskManager.java
+++ StorBackEnd/src/main/java/secCon/PickLayaDeti/tasks/interfaces/TaskManager.java
@@ -1,8 +1,10 @@
 package secCon.PickLayaDeti.tasks.interfaces;
 
+import java.io.IOException;
+
 public interface TaskManager {
 
     boolean check(String message);
 
-    void execute(String message);
+    void execute(String message) throws IOException;
 }
